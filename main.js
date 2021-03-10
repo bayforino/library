57c0b0548359e1d5344d59517466b4652d1d19e6
bayforino <will@bayford.design>
2021-03-10 09:56:44 +0000

    add persistence with localStorage

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const myLibrary = [];
+let myLibrary = [];
 const gridContainer = document.querySelector(".grid-container");
 const addButton = document.querySelector(".add-book-button");
 const emptyMessage = document.querySelector(".library-empty-message");
 const main = document.querySelector("main");
 const form = document.getElementById("book-form");
-
 
 let newBook;
 
@@ -21,9 +20,25 @@ function Book(title, author, length, readStatus) {
   this.author = author;
   this.length = length;
   this.readStatus = readStatus;
-  this.info = `<h2>${this.title}</h2><p> by ${this.author}.</p> <p>${
-    this.length
-  } pages long.</p>`;
+  this.info = `<h2>${this.title}</h2><p> by ${this.author}.</p> <p>${this.length} pages long.</p>`;
+}
+
+function initialGenerateLibrary() {
+  for (i = 0; i < myLibrary.length; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid-item");
+
+    gridContainer.appendChild(div);
+  }
+
+  const gridItems = document.querySelectorAll(`.grid-item`);
+
+  for (i = 0; i < myLibrary.length; i++) {
+    gridItems[i].innerHTML = myLibrary[i].info;
+    gridItems[i].setAttribute(`id`, i);
+    createReadText(gridItems);
+    createClearButton(gridItems);
+  }
 }
 
 function generateLibrary() {
@@ -44,29 +59,29 @@ function generateLibrary() {
 
 function createReadText(gridItems) {
   let readText = document.createElement("p");
-  readText.classList.add('read-text');
-  if (myLibrary[i].readStatus){
-  readText.textContent = "Read ✓";
-} else {
-  readText.textContent = "Not read";
-  readText.setAttribute(`id`, `not-read`);
-}
-readText.setAttribute(`data-attribute`, i);
-gridItems[i].appendChild(readText);
-readText.addEventListener("click", (e) => {
-  const readText = document.querySelectorAll('.read-text');
-  
-  if (myLibrary[e.target.dataset.attribute].readStatus){
-    myLibrary[e.target.dataset.attribute].readStatus = false;
-    readText[e.target.dataset.attribute].setAttribute(`id`, `not-read`);
-    readText[e.target.dataset.attribute].textContent = "Not read"
-  } else { 
-    myLibrary[e.target.dataset.attribute].readStatus = true;
-    readText[e.target.dataset.attribute].removeAttribute(`id`, `not-read`);
-    readText[e.target.dataset.attribute].textContent = "Read ✓"
-}
+  readText.classList.add("read-text");
+  if (myLibrary[i].readStatus) {
+    readText.textContent = "Read ✓";
+  } else {
+    readText.textContent = "Not read";
+    readText.setAttribute(`id`, `not-read`);
+  }
+  readText.setAttribute(`data-attribute`, i);
+  gridItems[i].appendChild(readText);
+  readText.addEventListener("click", (e) => {
+    const readText = document.querySelectorAll(".read-text");
 
-})
+    if (myLibrary[e.target.dataset.attribute].readStatus) {
+      myLibrary[e.target.dataset.attribute].readStatus = false;
+      readText[e.target.dataset.attribute].setAttribute(`id`, `not-read`);
+      readText[e.target.dataset.attribute].textContent = "Not read";
+    } else {
+      myLibrary[e.target.dataset.attribute].readStatus = true;
+      readText[e.target.dataset.attribute].removeAttribute(`id`, `not-read`);
+      readText[e.target.dataset.attribute].textContent = "Read ✓";
+    }
+    updateStorage();
+  });
 }
 
 function createClearButton(gridItems) {
@@ -78,8 +93,6 @@ function createClearButton(gridItems) {
   clearButton.addEventListener("click", removeBookFromLibrary);
 }
 
-
-
 function removeBookFromLibrary(e) {
   myLibrary.splice(e.target.dataset.attribute, 1);
   let itemToRemove = document.querySelector(
@@ -88,6 +101,7 @@ function removeBookFromLibrary(e) {
   gridContainer.removeChild(itemToRemove);
 
   resetDataTagOrder();
+  updateStorage();
 
   if (myLibrary.length === 0) {
     emptyMessage.style.display = "inherit";
@@ -109,6 +123,44 @@ function resetDataTagOrder() {
   }
 }
 
+function updateStorage() {
+  if (storageAvailable(`localStorage`)) {
+    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+  }
+}
+
+function loadStoredData() {
+  if (
+    localStorage.getItem("myLibrary") &&
+    localStorage.getItem("myLibrary") !== "[]"
+  ) {
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+    toggleEmptyMessage();
+    initialGenerateLibrary();
+  }
+}
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    let x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      (e.code === 22 ||
+        e.code === 1014 ||
+        e.name === "QuotaExceededError" ||
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
 function openForm() {
   form.style.display = "block";
 }
@@ -117,15 +169,19 @@ function closeForm() {
   form.style.display = "none";
 }
 
-
+function toggleEmptyMessage() {
+  if (myLibrary.length >= 0) {
+    emptyMessage.style.display = "none";
+  } else {
+    emptyMessage.style.display = "inherit";
+  }
+}
 
 addButton.addEventListener("click", openForm);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  if (myLibrary.length >= 0) {
-    emptyMessage.style.display = "none";
-  }
+  toggleEmptyMessage();
   let formValue = event.target.elements;
 
   newBook = new Book(
@@ -137,5 +193,8 @@ form.addEventListener("submit", (event) => {
 
   myLibrary.push(newBook);
   closeForm();
+  updateStorage();
   generateLibrary();
 });
+
+loadStoredData();
